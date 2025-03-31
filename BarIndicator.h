46385d9dc7b5d5fdd3ba3a595d7afa8351a65c51
ZiHEd <ziihed@gmail.com>
2025-03-31 15:16:19 +0200

    add Fraction lines to BarIndicator

--- BarIndicator.h
+++ BarIndicator.h
@@ -10,6 +10,7 @@ namespace aui {
 	{
 	public:
 		
+		bool showFractionLines = false;
 		bool isHorizontal = true;
 		int width, height;
 		int xOffset, yOffset;
@@ -19,11 +20,14 @@ namespace aui {
 		float maxFill=1;
 		float currentFill=0.5;
 		float fillFraction=0.5;
+		float fractionLineValue = 1;
+		int fractionLineCount = 0;
 		glm::vec4 fillColor;
 		glm::vec4 outlineColor;
+		glm::vec4 fractionLineColor;
 		QuadRenderer& renderer = Item::qr;
 		gui::Text text{};
-		//std::string textFormatString = "{}"; no apparently
+
 		bool showText = false;
 		bool showMax = true;
 		
@@ -108,12 +112,21 @@ namespace aui {
 				renderer.setQuadRendererMode(GL_TRIANGLES);
 				renderer.setColor(fillColor.r, fillColor.g, fillColor.b, fillColor.a);
 				renderer.render();
+				
+				if (showFractionLines) {
+					for (int i = 1;i < fractionLineCount;i++) {
+						renderer.setPos(xOffset+i*width/fractionLineCount, yOffset, 1, height);
+						renderer.setQuadRendererMode(GL_LINE_LOOP);
+						renderer.setColor(fractionLineColor.r, fractionLineColor.g, fractionLineColor.b, fractionLineColor.a);
+						renderer.render();
+					}
+				}
 
-				renderer.setPos(xOffset,yOffset, width, height);
+				renderer.setPos(xOffset, yOffset, width, height);
 				renderer.setQuadRendererMode(GL_LINE_LOOP);
 				renderer.setColor(outlineColor.r, outlineColor.g, outlineColor.b, outlineColor.a);
 				renderer.render();
-				
+
 				text.render(w);
 			}
 		}
@@ -147,12 +160,22 @@ namespace aui {
 			maxFill = fill;
 			fillFraction = currentFill / maxFill;
 			if (clampFill) clampFilling();
+			fractionLineValue = pow(5, (int)log10(maxFill-1));
+			fractionLineCount = maxFill / fractionLineValue;
+			if (fractionLineCount > 10)
+			{
+				fractionLineValue = pow(10, (int)log10(maxFill - 1));
+				fractionLineCount = maxFill / fractionLineValue;
+			}
 		}
 		void setOutlineColor(glm::vec4 color) {
 			outlineColor = color;
 		}
 		void setFillColor(glm::vec4 color) {
 			fillColor = color;
+		}
+		void setFractionLineColor(glm::vec4 color) {
+			fractionLineColor = color;
 		}
 
 		void offsetX(int offset) override { xOffset = offset; }
